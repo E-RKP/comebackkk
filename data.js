c4f072d5d5c548f23fb2529244916a3d5f2b3d2a
E-RKP <edobackup111@gmail.com>
2025-07-08 11:48:36 +0200

    version 3

--- data.js
+++ data.js
@@ -1,2 +1,34 @@
 const numerino = document.getElementById('numerino')
 const numerone = document.getElementById('numerone')
+const diminuire = document.getElementById('sx')
+const aggiungere = document.getElementById('dx')
+let counterpic = 0
+let counterbig = 0
+
+function diminuiree (){
+counterpic++;
+counterbig--;
+
+if (counterbig < 0){
+  counterbig = 0
+}
+
+return {
+  counterbig,
+  counterpic
+};
+}
+
+diminuire.addEventListener('click', ()=>{
+  diminuiree();
+  numerino.textContent = counterpic,
+  numerone.textContent = counterbig
+})
+
+aggiungere.onclick = function (){
+  counterbig++;
+  counterpic++;
+
+  numerino.textContent = counterpic,
+  numerone.textContent = counterbig
+}
